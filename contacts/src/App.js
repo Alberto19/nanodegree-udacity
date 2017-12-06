9c464daddc7da92200aeae4bd3273c09fd8fa84f
junior <junior@beetech.global>
2017-12-06 09:36:48 -0200

    feat(listContact): add new component ListContact

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListContacts from './ListContacts'
 
-class ContactList extends Component {
-  render() {
-
-    const people = this.props.contacts
-
-    return <ol>
-      { people.map(person => <li key={ person.name }>{ person.name }</li>) }
-    </ol>
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
-}
+]
 
 class App extends Component {
+  
   render() {
     return (
       <div>
-        <ContactList contacts= {[
-          { name: 'michael' },
-          { name: 'michael' },
-          { name: 'michael' }          
-        ]}/>
-
-        <ContactList contacts= {[
-          { name: 'test' },
-          { name: 'test' },
-          { name: 'test' }          
-        ]}/>  
+        <ListContacts contacts={contacts}/>
       </div>
     )
   }
 }
-    
 
 export default App;
